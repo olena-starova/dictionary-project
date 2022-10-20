95dabdcbf3f352530e5071029f17fdc85c0b0018
olena-starova <iverra@ukr.net>
2022-10-20 22:52:31 +0300

    Search Engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Dictionary</p>
-        <a href="#" className="btn btn-primary shadow">
-          Search
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <p>Dictionary</p>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Olena Starova</small>
+        </footer>
+      </div>
     </div>
   );
 }
